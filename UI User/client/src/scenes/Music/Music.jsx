c83637536bd9ca326a2bd3d60828e11899604d20
Salma EL HAZAL <salma.elhazal@gmail.com>
2024-10-18 13:14:53 +0100

    added music section

--- UI User/client/src/scenes/Music/Music.jsx
+++ UI User/client/src/scenes/Music/Music.jsx
@@ -1,9 +1,77 @@
-import React from 'react'
+import React, { useRef, useState } from "react";
+import Player from "./Player";
+import Song from "./Song";
+import "./styles/app.scss";
+import data from "./Playlist";
+import Library from "./Library";
+import Nav from "./Nav";
 
-const Music = () => {
+function App() {
+
+  const [songs, setSongs] = useState(data());
+  const [currentSong, setCurrentSong] = useState(songs[0]);
+  const [isPlaying, setIsPlaying] = useState(false);
+  const audioRef = useRef(null);
+  const [libraryStatus, setLibraryStatus] = useState(false);
+  const timeUpdateHandler = (e) => {
+    const current = e.target.currentTime;
+    const duration = e.target.duration;
+    const roundedCurrent = Math.round(current);
+    const roundedDuration = Math.round(duration);
+    const animationPercentage = Math.round(
+      (roundedCurrent / roundedDuration) * 100
+    );
+    setSongInfo({
+      ...songInfo,
+      currentTime: current,
+      duration,
+      animationPercentage,
+    });
+  };
+  const [songInfo, setSongInfo] = useState({
+    currentTime: 0,
+    duration: 0,
+    animationPercentage: 0,
+  });
+  const songEndHandler = async () => {
+    let currentIndex = songs.findIndex((song) => song.id === currentSong.id);
+    await setCurrentSong(songs[(currentIndex + 1) % songs.length]);
+    if (isPlaying) audioRef.current.play();
+  };
   return (
-    <div style={{ height: '90vh' }}>Music</div>
-  )
+    <div className={`App ${libraryStatus ? "library-active" : ""} `}>
+      <Nav libraryStatus={libraryStatus} setLibraryStatus={setLibraryStatus} />
+      <Library
+        audioRef={audioRef}
+        songs={songs}
+        setSongs={setSongs}
+        setCurrentSong={setCurrentSong}
+        isPlaying={isPlaying}
+        libraryStatus={libraryStatus}
+      />
+      <Song 
+        currentSong={currentSong}
+      />
+      <Player
+        audioRef={audioRef}
+        isPlaying={isPlaying}
+        setIsPlaying={setIsPlaying}
+        currentSong={currentSong}
+        setCurrentSong={setCurrentSong}
+        songs={songs}
+        setSongs={setSongs}
+        songInfo={songInfo}
+        setSongInfo={setSongInfo}
+      />
+      <audio
+        onTimeUpdate={timeUpdateHandler}
+        onLoadedMetadata={timeUpdateHandler}
+        ref={audioRef}
+        src={currentSong.audio}
+        onEnded={songEndHandler}
+      ></audio>
+    </div>
+  );
 }
 
-export default Music
+export default App;
---
+++ UI User/client/src/scenes/Music/Playlist.jsx
@@ -0,0 +1,27 @@
+import { v4 as uuidv4 } from "uuid";
+
+
+function chillHop() {
+  return [
+    {
+      name: "SIAMO L INPT",
+      cover: cover ,
+      artist: "Ultras Bleu Ines",
+      color: ["#06424D", "#A1C0EE"],
+      id: uuidv4(),
+      active: true,
+    },
+    {
+      name: "MAMEYO",
+      cover: cover ,
+      artist: "Ultras Bleu Ines",
+      color: ["#06424D", "#A1C0EE"],
+      id: uuidv4(),
+      active: false,
+    },
+   
+
+  ];
+}
+
+export default chillHop;
